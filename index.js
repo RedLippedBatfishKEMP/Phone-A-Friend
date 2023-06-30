00f9793654a28a99220b6598548b8019eb61f9bf
Morah Geist <114309093+morahgeist@users.noreply.github.com>
2023-06-30 16:45:01 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author
+ * @date
+ * @description
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import App from './App.jsx';
+
+
+render(<App />, document.getElementById('app'));
